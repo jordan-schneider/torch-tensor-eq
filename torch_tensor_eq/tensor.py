ea1dbecafca05f582261b04aa75ee7be811548cc
jordan-schneider <jordan.jack.schneider@gmail.com>
2020-03-30 12:55:06 -0500

    Fixes problems with hashing. Adds more relational operators.

--- torch_tensor_eq/tensor.py
+++ torch_tensor_eq/tensor.py
@@ -1,18 +1,45 @@
+import hashlib
+import json
+
 import torch
 
 
 class Tensor(torch.Tensor):
     """torch Tensor, except `__eq__` returns a bool, matching the python convention."""
 
-    def __eq__(self, other: torch.Tensor) -> bool:
-        """Returns true if all elements of other are equal to the corresponding elements of self."""
-        return torch.all(torch.eq(self, other))
+    def __hash__(self, *args, **kwargs):
+        # Deterministic hash by value
+        return hashlib.md5(json.dumps(self))
 
-    def __ne__(self, other: torch.Tensor) -> bool:
+    def __eq__(self, other) -> bool:  # type: ignore
+        """Returns true if all elements of other are equal to the corresponding elements of self."""
+        return isinstance(other, torch.Tensor) and bool(
+            torch.all(torch.eq(self, other)).item()
+        )
+
+    def __ne__(self, other) -> bool:  # type: ignore
         """Returns false if any element of other doesn't match a corresponding element of self. """
         return not self.__eq__(other)
+
+    def __lt__(self, other) -> bool:  # type: ignore
+        "A vector is not boolean less than another vector unless both are length 1."
+        if isinstance(other, Tensor) and self.shape == (1,) and other.shape == 1:
+            return self.item() < other.item()
+        return False
+
+    def __le__(self, other) -> bool:  # type: ignore
+        return self == other
+
+    def __gt__(self, other) -> bool:  # type: ignore
+        if isinstance(other, Tensor) and self.shape == (1,) and other.shape == 1:
+            return self.item() > other.item()
+        return False
+
+    def __ge__(self, other) -> bool:  # type: ignore
+        return self == other
 
 
 def eq(tensor1: Tensor, tensor2: Tensor) -> Tensor:
     """Returns an elementwise equality between two Tensors."""
-    return torch.eq(tensor1, tensor2)
+    out: torch.Tensor = torch.eq(tensor1, tensor2)
+    return Tensor(out)
